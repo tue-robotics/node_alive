74719853c7667dfa6fa8d20826b1c2737e6d0916
Kevin Dang <kevin.acnowlogia@gmail.com>
2017-12-09 14:22:23 +0100

    feature(amigo free mode restart) Changed subscriber to service and added doc

--- amigo_free_mode_restart.py
+++ amigo_free_mode_restart.py
@@ -5,26 +5,42 @@ import rospy
 import rospkg
 import os
 from std_msgs.msg import String
+from node_alive.srv import AutoStarterCommand, AutoStarterCommandRequest, AutoStarterCommandResponse
 
 class AutoStarter(object):
     """
-    Launches amigo_free_mode and waits for it to either shutdown or to restart.
+    Launches amigo_free_mode and waits for a command to either shutdown or to restart.
     """
     def __init__(self):
         """
-        Launches amigo_free_mode and starts a makes a subscriber that listens when it should
-        shutdown or restart. When the node stops it will also shutdown the launch file.
+        Creates a service over which the command will be received, next amigo free mode is started and keeps running
+        until it gets a command to either shutdown or to restart.
         """
-
+        self._command_service = rospy.Service('auto_starter_command', AutoStarterCommand, self._handle_auto_starter_command)
         self.start()
-
-        rospy.Subscriber('action', String, self._callback)
-
         rospy.spin()
-
         self.launch.shutdown()
 
+    def _handle_auto_starter_command(self, req):
+        """
+        All request from the service are passed into this function and depending on the value it will either shutdown
+        or restart the launch file.
+        :param req: the command request that the service receives from the client.
+        :return: returns the integer 1  when function finishes successful.
+        """
+        if req.command == AutoStarterCommandRequest.STOP:
+            self.stop()
+            rospy.loginfo("Performed shutdown")
+        elif req.command == AutoStarterCommandRequest.RESTART:
+            self.stop()
+            self.start()
+            rospy.loginfo("Performed restart")
+        return AutoStarterCommandResponse(AutoStarterCommandResponse.SUCCEEDED)
+
     def start(self):
+        """
+        Finds the right directory of the launch file of free mode and launches the file.
+        """
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
 
@@ -33,25 +49,13 @@ class AutoStarter(object):
 
         self.launch = roslaunch.parent.ROSLaunchParent(uuid, [
             os.path.join(rospack.get_path('amigo_bringup'), "launch", "state_machines", "free_mode.launch")])
-
         self.launch.start()
 
     def stop(self):
-
+        """
+        Shuts the launch file down.
+        """
         self.launch.shutdown()
-
-    def _callback(self, msg):
-        """
-        :param msg: Is a string with either shutdown or restart.
-        """
-
-        if msg.data == "shutdown":
-            self.stop()
-            print "Performed {}".format(msg.data)
-        elif msg.data == "restart":
-            self.stop()
-            self.start()
-            print "Performed {}".format(msg.data)
 
 if __name__ == "__main__":
     rospy.init_node('auto_starter', anonymous=False)
